7375c28c7873da9e35ceb4980bc376129b117891
Anthony Tesoriero <anttesoriero@gmail.com>
2019-10-13 11:43:05 -0400

    Updating Comments

--- Programming-Languages-Python/1. Beginner's Program/Beginners-Assignment.py
+++ Programming-Languages-Python/1. Beginner's Program/Beginners-Assignment.py
@@ -1,4 +1,4 @@
-# Anthony Tesoriero, Joseph Salemo, Joshua Lunsk, October 9 2019, Beginner Assignment for Programming Languages
+# Anthony Tesoriero, Joseph Salemo, Joshua Lunsk, October 9 2019, Beginner Assignment
 # Write a program that will read two numbers and will compute the sum of those two numbers.
 # (If you are working in a functional language, you may write a function that takes two numbers
 # as input parameters and returns the sum of the two numbers.)
