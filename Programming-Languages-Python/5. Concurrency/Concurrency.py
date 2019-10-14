f0e9a3803cb0c21cf0e41fa0c51359d6c8bfa440
Anthony Tesoriero <anttesoriero@gmail.com>
2019-10-14 14:31:34 -0400

    Concurrency in progress

--- Programming-Languages-Python/5. Concurrency/Concurrency.py
+++ Programming-Languages-Python/5. Concurrency/Concurrency.py
@@ -7,72 +7,102 @@
 
 import random
 
+
 # ---------- Initialization ---------- #
+class Concurrency:
 
-# Size of grid (n x n)
-# n = input("What is the size of your grid? (n x n) ")
-n = 3
-# agent1 position is NW corner
-agent1 = ["Agent 1", 1]
-# agent2 position is SE corner
-agent2 = ["Agent 2", n**2]
-# Number of gems found out of 4
-foundGems = 0
+	def __init__(self):
 
-# Gets 4 unique random nums
-rands = []
-for i in range(8):
-	rand = random.randint(1,(n**2)+1)
-	if rand not in rands: 
-		rands.append(rand)
+		# Size of grid (n x n)
+		self.n = int(input("What is the size of your grid? (n x n) "))
+		self.version = int(input("Input '1' for starting in opposite corners, '2' for random movements: "))
 
-# gems [name, position]
-emerald = ["an emerald", rands[0]]
-crown = ["a crown", rands[1]]
-coin = ["a coin", rands[2]]
-book = ["a rare book", rands[3]]
+		# Initialize agents
+		self.agent1 = ["Agent 1", 0]
+		self.agent2 = ["Agent 2", 0]
+		self.foundGems = 0
 
-# List of gems
-gems = [emerald, crown, coin, book]
+		# Gets 4 unique random nums
+		rands = []
+		for i in range(8):
+			rand = random.randint(1, (self.n ** 2) + 1)
+			if rand not in rands:
+				rands.append(rand)
 
-# ---------- Functions ---------- #
+		# List of gems [name, position]
+		self.gems = [["an emerald", rands[0]], ["a crown", rands[1]], ["a coin", rands[2]], ["a rare book", rands[3]]]
 
-# Checks if direction attempt is valid
-# dir: 1 = north, 2 = east, 3 = south, 4 = west
-def checkDir(self, agent, dir):
-	# North
-	if dir is 1 and agent[1] - n < 1:
-		return False
+	# ---------- Functions ---------- #
 
-	# East
-	if dir is 2 and agent[1] % n is 0:
-		return False
+	def setType(self):
+		if self.version is 1:
+			# agent1 position is NW corner
+			agent1 = ["Agent 1", 1]
+			# agent2 position is SE corner
+			agent2 = ["Agent 2", self.n ** 2]
+		else:
+			# agent1 and agent2 position random
+			self.agent1[1] = random.randint(1, 9)
+			self.agent2[1] = random.randint(1, 9)
 
-	# South
-	if dir is 3 and agent[1] + n > n**2:
-		return False
+	# Checks if direction attempt is valid
+	# dir: 1 = north, 2 = east, 3 = south, 4 = west
+	def checkDir(self, agent, dir):
+		# North
+		if dir is 1 and agent[1] - self.n < 1:
+			return False
+		# East
+		if dir is 2 and agent[1] % self.n is 0:
+			return False
+		# South
+		if dir is 3 and agent[1] + self.n > self.n ** 2:
+			return False
+		# West
+		if dir is 4 and (agent[1] - 1) % self.n is 0:
+			return False
+		# No failures
+		return True
 
-	# West
-	if dir is 4 and (agent[1] - 1) % n is 0:
-		return False
-	
-	# No failures
-	return True
+	# Moves agent
+	# dir: 1 = north, 2 = east, 3 = south, 4 = west
+	def move(self, agent, dir):
+		if dir is 1 and self.checkDir(agent, dir):
+			agent[1] -= self.n
+		if dir is 2 and self.checkDir(agent, dir):
+			agent[1] += 1
+		if dir is 3 and self.checkDir(agent, dir):
+			agent[1] += self.n
+		if dir is 4 and self.checkDir(agent, dir):
+			agent[1] -= 1
 
-# Finds gem with agent
-def findGem(gem, agent):
-	global foundGems
-	foundGems	+= 1
-	print(agent[0], "found", gem[0], "in room", gem[1])
-	print(4-foundGems, "gems left.")
-	gem[1] = -1
+	# Defined movement
+	def moveDir(self):
+		return 0
 
-# Find if agent position equals gems
-def gemCheck(agent):
-	for i in gems:
-		if agent[1] is i[1]:
-			findGem(i, agent)
-			
+	# Random movement
+	def randomMoveDir(self):
+		return random.randint(1, 4)
+
+	# Finds gem with agent
+	def findGem(self, gem, agent):
+		self.foundGems += 1
+		print(agent[0], "found", gem[0], "in room", gem[1])
+		print(4 - self.foundGems, "gems left.")
+		gem[1] = -1
+
+	# Find if agent position equals gems
+	def gemCheck(self, agent):
+		for gem in self.gems:
+			if agent[1] is gem[1]:
+				self.findGem(self, gem, agent)
+
+	# Run
+	def run(self):
+		while self.foundGems != 4:
+			self.move(self.agent1, 1)
+
 # ---------- Driver ---------- #
 
+# Run Concurrency
 
+game = Concurrency()
