87109cdb387def83f90d0da07f0a93e4d55f667f
Anthony Tesoriero <anttesoriero@gmail.com>
2019-10-16 16:11:28 -0400

    Complete, to be submitted

--- Programming-Languages-Python/2. Simple Repitition/Simple-Repetition.py
+++ Programming-Languages-Python/2. Simple Repitition/Simple-Repetition.py
@@ -1,4 +1,5 @@
 # Programming Languages Fall 2019 Python Group, Simple Repetition
+# Status: Complete, to be submitted
 print("Input each of your numbers, pressing return after each one. Enter 'x' when done.")
 sum = 0.0
 n = 0
