6515b9f810a29078cb2d99d6d280b14a62e5bc87
Anthony Tesoriero <anttesoriero@gmail.com>
2019-10-16 16:14:52 -0400

    Complete, to be submitted

--- Programming-Languages-Python/2. Simple Repitition/Simple-Repetition.py
+++ Programming-Languages-Python/2. Simple Repitition/Simple-Repetition.py
@@ -1,8 +1,12 @@
-# Programming Languages Fall 2019 Python Group, Simple Repetition
+# Anthony Tesoriero, Joseph Salemo, Joshua Lunsk, October 9 2019, Simple Repetition
 # Status: Complete, to be submitted
+
+# Prints statement for user inpit
 print("Input each of your numbers, pressing return after each one. Enter 'x' when done.")
 sum = 0.0
 n = 0
+
+# Loops through input with summation
 while n != "x":
 	n = input("Input: ")
 	if n != "x":
