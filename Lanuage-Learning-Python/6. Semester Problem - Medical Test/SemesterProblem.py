9c9a30033c2cec041d1956fc2ca44ccbcbfb9260
Anthony Tesoriero <anttesoriero@gmail.com>
2019-11-14 13:06:56 -0500

    Updated Semester Problem

---
+++ Lanuage-Learning-Python/6. Semester Problem - Medical Test/SemesterProblem.py
@@ -0,0 +1,7 @@
+# Anthony Tesoriero, Joseph Salemo, Joshua Lunsk, November 14 2019, Semester Problem - Medical Test
+# Status: Started
+# Check problem on https://github.com/anttesoriero/Language-Learning-Python/blob/master/SEMESTERHW.md
+
+# Input data from file
+
+
